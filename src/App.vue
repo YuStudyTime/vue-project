028261efe5912d61515958923761b7b32bf4a545
yu jie <yujieworking@gmail.com>
2024-01-30 15:18:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+function callback(n) {
+  console.log("App" + n)
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">    
+      <!-- // 这里是父组件往子组件中传递数据 -->
+      <!-- // 这里测试几种组件参数传递方式 -->
+      <!-- // 1. 之间传递 msg String类型 -->
+      <!-- <HelloWorld msg="Vue3 Funcation Test Project!" /> -->
+      <!-- // 2. 传递对象 obj 对象类型 注意这里的：号  错误的写法会导致传递过去的数据是字符串-->
+      <HelloWorld @submitBtn="callback" title="WenPei big SB!" :value="123" :obj="{title: 'WenPei', value: 0}" />
+  
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/test">Test</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<script setup>
+// 注册提交事件
+const emit = defineEmits('submitBtn')
+import { ref } from 'vue'
+const count = ref(0)
+// 组件需要先声明是否支持参数传递
+const props = defineProps({
+  // msg: {
+  //   type: String,
+  //   required: true
+  // }
+  title: String,
+  value: Number,
+  obj: {
+    type: Object,
+  }
+})
+
+// 实现提交事件
+function submitBtn(){
+  count.value++
+  console.log(count.value)
+  emit('submitBtn', count.value)
+}
+
+console.log(props)
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ "收到了父组件的传参 = " + props.title + "&" + props.value }}</h1>
+    <!-- <button @click="$emit('submitBtn', count)"> {{ count }} </button> -->
+    <button @click="submitBtn()"> {{ count }} </button>
+    <h3>
+      This is a Web to Test Vue3 funcation
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">props.title</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
